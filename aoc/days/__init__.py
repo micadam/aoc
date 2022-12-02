4f859c2d6a2a39b111cff0ec967b85e12fd0f3a4
Adam Michalski <aadam.michalski@gmail.com>
2022-12-02 10:32:07 +0000

    Dodaj dzień drugi
    Jest to drugi dzień

--- aoc/days/__init__.py
+++ aoc/days/__init__.py
@@ -2,9 +2,11 @@ from typing import Dict, Type
 
 from aoc.days.day import Day
 from aoc.days.day1 import Day1
+from aoc.days.day2 import Day2
 
 DAYS: Dict[int, Type[Day]] = {
-    1: Day1
+    1: Day1,
+    2: Day2,
 }
 
 __all__ = ['DAYS']
