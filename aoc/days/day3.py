c89a8b081348471da94509640ced8d74e124152d
Adam Michalski <aadam.michalski@gmail.com>
2022-12-03 17:05:13 +0000

    Προσθέστε την τρίτη μέρα
    Ναί, τρίτη μέρα

--- aoc/days/day3.py
+++ aoc/days/day3.py
@@ -6,7 +6,25 @@ class Day3(Day):
         super().__init__(3, test)
 
     def part1(self):
-        raise NotImplementedError
+        return sum(
+            self.__get_priority(next(
+                i for i in rucksack
+                if i in rucksack[:(size := len(rucksack) // 2)]
+                and i in rucksack[size:]))
+            for rucksack in self.lines
+        )
 
     def part2(self):
-        raise NotImplementedError
+        return sum(
+            self.__get_priority(next(
+                c for c in self.lines[i]
+                if all(c in r for r in self.lines[i + 1:i + 3])))
+            for i in range(0, len(self.lines), 3)
+        )
+
+    def __get_priority(self, char: str):
+        if char.islower():
+            return ord(char) - ord('a') + 1
+        if char.isupper():
+            return ord(char) - ord('A') + 27
+        raise ValueError(f"Not a letter: {char}")
