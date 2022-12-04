0a249c527df8dc54562c5112bd326509a811c5fa
Adam Michalski <aadam.michalski@gmail.com>
2022-12-04 11:37:11 +0000

    Ajouter le quatrième jour
    Oui.

--- aoc/days/day4.py
+++ aoc/days/day4.py
@@ -1,3 +1,5 @@
+import re
+
 from aoc.days.day import Day
 
 
@@ -6,7 +8,12 @@ class Day4(Day):
         super().__init__(4, test)
 
     def part1(self):
-        raise NotImplementedError
+        return sum(1 for line in self.lines
+                   if (bounds := list(map(int, re.split(r"[,-]", line))))
+                   and (bounds[0] <= bounds[2] and bounds[1] >= bounds[3] or
+                        bounds[0] >= bounds[2] and bounds[1] <= bounds[3]))
 
     def part2(self):
-        raise NotImplementedError
+        return sum(1 for line in self.lines
+                   if (bounds := list(map(int, re.split(r"[,-]", line))))
+                   and not (bounds[2] > bounds[1] or bounds[0] > bounds[3]))
