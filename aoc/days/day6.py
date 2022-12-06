532be9504224852b590bfef6b646fec6b3676264
Adam Michalski <aadam.michalski@gmail.com>
2022-12-06 10:01:59 +0000

    Add hozzá a hatodik napot
    jó

--- aoc/days/day6.py
+++ aoc/days/day6.py
@@ -6,7 +6,14 @@ class Day6(Day):
         super().__init__(6, test)
 
     def part1(self):
-        raise NotImplementedError
+        return self.__fin_n_unique(4)
 
     def part2(self):
-        raise NotImplementedError
+        return self.__fin_n_unique(14)
+
+    def __fin_n_unique(self, n):
+        inp = self.lines[0]
+        ans = n - 1
+        while len(set(inp[ans - n + 1:ans + 1])) < n:
+            ans += 1
+        return ans + 1
