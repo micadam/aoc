df690deeb8713d73a4ccb1a94bd350d665681448
Adam Michalski <aadam.michalski@gmail.com>
2022-12-18 05:27:13 +0000

    열여덟 번째 날을 더하다
    B)

--- aoc/days/day18.py
+++ aoc/days/day18.py
@@ -1,4 +1,15 @@
+from collections import deque
+
 from aoc.days.day import Day
+
+DPOS = [
+    (-1, 0, 0),
+    (1, 0, 0),
+    (0, -1, 0),
+    (0, 1, 0),
+    (0, 0, -1),
+    (0, 0, 1)
+]
 
 
 class Day18(Day):
@@ -6,8 +17,53 @@ class Day18(Day):
         super().__init__(18, test)
 
     def part1(self):
-        raise NotImplementedError
+        cubes = set()
+        for line in self.lines:
+            cubes.add(tuple(map(int, line.split(","))))
+        ans = 0
+        for a, b, c in cubes:
+            for da, db, dc in DPOS:
+                ans += ((a + da, b + db, c + dc) not in cubes)
+        return ans
 
     def part2(self):
-        raise NotImplementedError
-
+        cubes = set()
+        groups = {}
+        for line in self.lines:
+            cubes.add(tuple(map(int, line.split(","))))
+        a_range = range(min(na for na, _, _ in cubes) - 1,
+                        max(na for na, _, _ in cubes) + 2)
+        b_range = range(min(nb for _, nb, _ in cubes) - 1,
+                        max(nb for _, nb, _ in cubes) + 2)
+        c_range = range(min(nc for _, _, nc in cubes) - 1,
+                        max(nc for _, _, nc in cubes) + 2)
+        group = 0
+        visited = set()
+        for a in a_range:
+            for b in b_range:
+                for c in c_range:
+                    if (a, b, c) in cubes or (a, b, c) in visited:
+                        continue
+                    group += 1
+                    to_visit = deque([(a, b, c)])
+                    while to_visit:
+                        na, nb, nc = to_visit.popleft()
+                        if ((na, nb, nc)) in visited:
+                            continue
+                        visited.add((na, nb, nc))
+                        groups[(na, nb, nc)] = group
+                        for da, db, dc in DPOS:
+                            nna, nnb, nnc = na + da, nb + db, nc + dc
+                            if nna not in a_range \
+                                    or nnb not in b_range \
+                                    or nnc not in c_range \
+                                    or (nna, nnb, nnc) in cubes \
+                                    or (nna, nnb, nnc) in to_visit:
+                                continue
+                            to_visit.append((nna, nnb, nnc))
+        ans = 0
+        for a, b, c in cubes:
+            for da, db, dc in DPOS:
+                cube = (a + da, b + db, c + dc)
+                ans += (cube not in cubes and groups[cube] == 1)
+        return ans
