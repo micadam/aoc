19b39b13838f54d0de2d1e689fefb1a6a57b445e
Adam Michalski <aadam.michalski@gmail.com>
2025-12-01 10:46:26 +0000

    1
    :)

--- aoc/days_2025/day01.py
+++ aoc/days_2025/day01.py
@@ -1,4 +1,24 @@
+from functools import reduce
+
 from aoc.day import Day
+
+
+def consume(acc: int, score, next: str, only_final_click=True) -> tuple[int, int]:
+    sign = 1 if next[0] == "R" else -1 if next[0] == "L" else None
+    if not sign:
+        raise ValueError("Unknown sigh")
+    new_acc_raw = acc + sign * int(next[1:])
+    new_acc = new_acc_raw % 100
+
+    if only_final_click:
+        score += new_acc == 0
+    else:
+        step = 1 if new_acc_raw > acc else -1
+        for pos_0 in range(acc + step, new_acc_raw + step, step):
+            if pos_0 % 100 == 0:
+                score += 1
+
+    return new_acc, score
 
 
 class Day1(Day):
@@ -6,7 +26,13 @@ class Day1(Day):
         super().__init__(1, test)
 
     def part1(self):
-        return -1
+        return reduce(
+            lambda acc, next: consume(acc[0], acc[1], next), self.lines, (50, 0)
+        )[1]
 
     def part2(self):
-        return -1
+        return reduce(
+            lambda acc, next: consume(acc[0], acc[1], next, only_final_click=False),
+            self.lines,
+            (50, 0),
+        )[1]
