c50f08e3c65b7bc13b86988e3dc726a32146f80b
Adam Michalski <aadam.michalski@gmail.com>
2025-12-09 14:06:20 +0000

    9
    D:

--- aoc/days_2025/day09.py
+++ aoc/days_2025/day09.py
@@ -1,3 +1,5 @@
+import shapely
+
 from aoc.day import Day
 
 
@@ -6,7 +8,28 @@ class Day9(Day):
         super().__init__(9, test)
 
     def part1(self):
-        return -1
+        points = [
+            (int(nums[0]), int(nums[1]))
+            for nums in [line.split(",") for line in self.lines]
+        ]
+        return max(
+            (abs(p1[0] - p2[0]) + 1) * (abs(p1[1] - p2[1]) + 1)
+            for p1 in points
+            for p2 in points
+        )
 
     def part2(self):
-        return -1
+        points = [
+            (int(nums[0]), int(nums[1]))
+            for nums in [line.split(",") for line in self.lines]
+        ]
+
+        points.append(points[0])
+
+        polygon = shapely.Polygon(points)
+        return max(
+            (abs(p1[0] - p2[0]) + 1) * (abs(p1[1] - p2[1]) + 1)
+            for p1 in points
+            for p2 in points
+            if polygon.contains(shapely.box(p1[0], p1[1], p2[0], p2[1]))
+        )
