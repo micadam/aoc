59861712407beafbefc13a824dfdeea7bda1f259
Adam Michalski <aadam.michalski@gmail.com>
2025-12-12 10:41:28 +0000

    12
    :|

--- aoc/days_2025/day12.py
+++ aoc/days_2025/day12.py
@@ -5,8 +5,32 @@ class Day12(Day):
     def __init__(self, test: bool) -> None:
         super().__init__(12, test)
 
+    def parse(self):
+        presents = {}
+        while (line := self.lines.pop(0)).endswith(":"):
+            prsent_id = int(line.split(":")[0])
+            present_lines = self.lines[:3]
+            self.lines = self.lines[4:]  # skip empty lines as well
+            presents[prsent_id] = present_lines
+
+        trees = []
+        for line in self.lines:
+            size, present_ids = line.split(": ")
+            width, length = size.split("x")
+            width, length = int(width), int(length)
+            present_ids = [int(id) for id in present_ids.split()]
+            assert len(present_ids) == len(presents)
+            trees.append((width, length, present_ids))
+
+        return presents, trees
+
     def part1(self):
-        return -1
+        _, trees = self.parse()
+        # Just give each present a full 3x3 box I guess.
+        return sum(
+            (width // 3) * (length // 3) > sum(presents)
+            for width, length, presents in trees
+        )
 
     def part2(self):
-        return -1
+        return "Merry Christmas!"
